590989de396e7547681d882bea9e7e21670b4a8e
Sinuee Hernández <esteban.hernandez@definityfirst.com>
2016-05-02 22:10:38 -0500

    inicial

---
+++ ConductoresProfesionales/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConductoresProfesionales.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ConductoresProfesionales/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConductoresProfesionales.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
